d97df797eec367212feb7da89c292fc785d8a568
Your_K <kafu@yourk.me>
2019-10-09 02:46:52 +0900

    update
    - 지역별 검색이 가능해졌습니다(한국어 한정)
    - 미세먼지, 초미세먼지, 오존지수의 악화에 따른 미표기 버그를 없애버렸습니다.
    - 정보가 단 하나도 없을 경우 status: no information을 반환합니다.

--- parse.js
+++ parse.js
@@ -5,18 +5,19 @@ const app = express();
 
 const pData = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%82%A0%EC%94%A8";
 
-async function weather(type) {
-    let $ = cheerio.load((await request(pData)).body);
+async function weather(city) {
+    let $ = cheerio.load((await request((typeof city == "undefined")?pData:pData+encodeURI(city))).body);
     let data = $('div[class=weather_box]');
     let dust = [];
 
-    data.find('dd[class=lv1]').each(function() {
+    data.find('dl[class=indicator]').find('dd').each(function() {
         if($(this).find('dd > span[class=num]').text() != "") {
             dust.push($(this).find('dd > span[class=num]').text());
         }
     });
 
     let information = {
+        "information": "success",
         "area": data.find('span[class=btn_select]').find('em').text(),
         "temp": data.find('p[class=info_temperature]').find('span[class=todaytemp]').html(),
         "dust": dust.shift(),
@@ -26,11 +27,22 @@ async function weather(type) {
         "rainfall" : data.find('span[class=rainfall] > em > span[class=num]').text() + "mm"
     };
 
+    if(information.rainfall == 'mm') {
+        information.rainfall = '0mm';
+    }
+
+    if(data.html() == null) {
+        information = {status: "no information"};
+    }
+
     return information;
 }
 
 app.get('/', async (req,res)=>{
     res.send((await weather()));
+});
+app.get('/:city', async (req,res)=>{
+    res.send((await weather(req.params.city)));
 });
 
 app.listen(80);
